1c3930f2c752b1ab75ffb8009c157d01444b3d4a
eruera <eruerame@gmail.com>
2017-03-11 22:02:19 +0800

    Add

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
-    minifycss = require('gulp-minify-css')
+    minifycss = require('gulp-minify-css')，
+    imagemin = require('gulp-imagemin');
 
 gulp.task('scripts',function(){
     gulp.src('js/*.js')
-
         .pipe(uglify())
         .pipe(rename('app.min.js'))
         .pipe(gulp.dest('js/'));
@@ -17,4 +17,15 @@ gulp.task('styles',function(){
         .pipe(gulp.dest('minCSS'));
 });
 
-gulp.task('default',['scripts','styles']);
+gulp.task('watch',function(){
+    gulp.watch('js/*.js',['scripts']);
+    gulp.watch('css/*.css',['styles']);
+});
+
+gulp.task('imagemin', function(){
+    gulp.src('src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('default',['scripts','styles','watch']);
